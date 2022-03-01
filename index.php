d23fa0ff6af2843fdc0590c2d7ee84090193e85f
Anthony Smidh Urrego Pineda <35052057+anthourrego@users.noreply.github.com>
2022-03-01 15:29:22 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+  include("clases/stringToken.php");
+  include("clases/lexer.php");
+
+  $txt = '';
+  $fp = fopen("upload/source.lx", "r");
+
+  while (!feof($fp)) {
+    $txt .= fgets($fp);
+  }
+
+  fclose($fp);
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,11 +21,21 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
     <title>Analizador Léxico</title>
   </head>
   <body class="container">
-    <div class="p-3 my-3 text-center bg-light rounded-3">
+    <div class="p-3 my-3 bg-light rounded-3">
       <h1 class="display-5 fw-bold">Analizador Léxico</h1>
+      <br>
+      <div class="row">
+        <div class="col-6 text-start">
+          <?=  "<b>ENTRADA (source.lx)</b>: <br><pre>{$txt}</pre>" ?>
+        </div>
+        <div class="col-6 overflow-auto contenedor-tabla">
+          <?php $lexer = new Lexer($txt); ?> 
+        </div>
+      </div>
     </div>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
   </body>
